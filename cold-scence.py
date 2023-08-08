5a82e55d61dfc08d19f12210f7a0fed22c733877
zixiaojin66 <126325476+zixiaojin66@users.noreply.github.com>
2023-08-08 21:48:22 +0800

    Update cold-scence.py

--- cold-scence.py
+++ cold-scence.py
@@ -13,7 +13,7 @@ from sklearn.neighbors import kneighbors_graph
 from torch_geometric.data import Data, DataLoader
 
 from Net import *
-from smiles2vector import load_drug_smile, convert2graph
+from vector import load_drug_smile, convert2graph
 from utils import *
 
 raw_file = 'data_ICS/raw_frequency_750.mat'
